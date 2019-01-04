514a70718b66cec2333fa3c47215ef9a35f0e8ae
石建涛 <vannisher@yahoo.com>
2019-01-04 14:04:01 +0800

    updated app

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
 const express = require('express')
 const expressLayouts =  require('express-ejs-layouts')
+const mongoose = require('mongoose')
+const passport = require('passport');
+const flash = require('connect-flash')
+const session =  require('express-session')
 const app = express()
 
+//Passport Config
+require('./config/passport')(passport);
 
+//DB Config
+const db = require('./config/keys').MongoURI;
+
+//Conenct to Mongo
+mongoose.connect('mongodb://127.0.0.1:27017/Mong',{ useNewUrlParser: true})
+    .then(() => console.log('MongDB Connected...'))
+    .catch(err => console.log(err));
 
 //ejs
 app.use(expressLayouts)
 app.set('view engine','ejs')
+
+//bodyparser
+app.use(express.urlencoded( { extended: false } ));
+
+//Express session
+app.use(session({
+   secret: 'secret',
+   resave: true,
+   saveUninitialized: true
+}))
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Content flash
+app.use(flash());
+
+//Global Vars
+app.use((req,res,next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 
 //Routes
 app.use('/',require('./routes/index'));
